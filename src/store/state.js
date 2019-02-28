70a0384cbe1e3d1f8b616a9f8b688115b8b5bcbb
Ruhan de Oliveira Baiense <rdgitarra@gmail.com>
2019-02-28 12:40:44 -0300

    gerando build de deploy no heroku

--- src/store/state.js
+++ src/store/state.js
@@ -1,7 +1,8 @@
 // https://vuex.vuejs.org/en/state.html
 
 export default {
-  API_URL: 'http://127.0.0.1:8000/api/',
+  // API_URL: 'http://127.0.0.1:8000/api/',
+  API_URL: 'http://clean-sistema-os-api.herokuapp.com/api/',
 
   drawer: true,
   toolbarBackUrl: null,
