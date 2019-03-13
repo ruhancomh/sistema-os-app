f928827bebd946ce3f2284ceaafda1d26877ead2
Ruhan de Oliveira Baiense <rdgitarra@gmail.com>
2019-03-12 21:09:18 -0300

    gerando build pro heroku

--- src/store/state.js
+++ src/store/state.js
@@ -2,9 +2,9 @@
 
 export default {
   // API_URL: 'http://127.0.0.1:8000/api/',
-  //API_URL: 'https://clean-sistema-os-api.herokuapp.com/api/',
+  API_URL: 'https://clean-sistema-os-api.herokuapp.com/api/',
 
-  API_URL: process.env.API_URL,
+  // API_URL: process.env.API_URL,
 
   drawer: true,
   toolbarBackUrl: null,
